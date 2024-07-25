61c1f7cab87321b25785c28ed7632a8dd85d3207
itomo8849 <tomolllllllo@gmail.com>
2024-07-25 16:44:31 +0900

    indent

--- image/assets/js/main.js
+++ image/assets/js/main.js
@@ -10,11 +10,22 @@ ScrollTrigger.batch(triggerEls, {
   onEnter: (elements) => {
     const tl = gsap.timeline({ defaults: { ease: "ease" } });
     elements.forEach((el, i) => {
-      tl.to(el, { opacity: 1, duration: 0.6, stagger: 0.3 }, i * 0.2).to(
-        el,
-        { filter: "grayscale(0%) brightness(100%)", duration: 0.6 },
-        `-=${0.6 - 0.3}`
-      );
+      tl
+        .to(el,
+          {
+            opacity: 1,
+            duration: 0.6,
+            stagger: 0.3
+          },
+          i * 0.2
+        )
+        .to(el,
+          {
+            filter: "grayscale(0%) brightness(100%)",
+            duration: 0.6
+          },
+          `-=${0.6 - 0.3}`
+        );
     });
   },
   onLeaveBack: (elements) => {
@@ -29,36 +40,3 @@ ScrollTrigger.batch(triggerEls, {
     });
   },
 });
-
-// const triggerEls = document.querySelectorAll(".js_trigger");
-// gsap.set(triggerEls, {
-//   opacity: 0,
-//   filter: "grayscale(100%) brightness(150%)",
-// });
-// ScrollTrigger.batch(triggerEls, {
-//   start: "top 80%",
-//   onEnter: (elements) =>
-//     gsap.to(elements, {
-//       opacity: 1,
-//       filter: "grayscale(0%) brightness(100%)",
-//       stagger: 0.3,
-//     }),
-//   onLeaveBack: (elements) =>
-//     gsap.set(elements, { opacity: 0, overwrite: true }),
-// });
-
-// triggerEls.forEach((triggerEl) => {
-//   ScrollTrigger.create({
-//     trigger: triggerEl,
-//     start: "top 70%",
-//     end: "bottom 10%",
-//     toggleClass: {
-//       targets: triggerEl,
-//       className: "is_active",
-//     },
-//     // once: true,
-//     markers: true,
-//   });
-// });
-
-// gsap.set(triggerEls, { y: 100, opacity: 0 });
